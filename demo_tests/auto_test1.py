1eeda372347b7f5c26ac135a9f8e0c82e7e78522
JacekMajerski <134397104+JacekMajerski@users.noreply.github.com>
2024-04-16 07:36:23 +0200

    Delete demo_tests directory

--- demo_tests/auto_test1.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-
-driver.get('https://demobank.jaktestowac.pl/logowanie_etap_1.html')
-title = driver.title
-print(title)
-assert 'Demobank - Bankowość Internetowa - Logowanie' == title
-driver.quit()
